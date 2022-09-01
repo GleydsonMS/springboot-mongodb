69a5e5e067c753970fe6288cf190fc61af4e8e5e
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2022-09-01 08:45:25 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/gleydson/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.gleydson.workshopmongo.services;
+
+import com.gleydson.workshopmongo.domain.User;
+import com.gleydson.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
