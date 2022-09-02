af1eb1dfc847c9f0603cdcf9e202442f92bacd16
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2022-09-02 07:23:56 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gleydson/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.gleydson.workshopmongo.services;
+
+import com.gleydson.workshopmongo.domain.Post;
+import com.gleydson.workshopmongo.repository.PostRepository;
+import com.gleydson.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
