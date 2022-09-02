084d2c49b7115421e7fad732ca497352662c5901
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2022-09-01 21:58:59 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/gleydson/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.gleydson.workshopmongo.repository;
+
+import com.gleydson.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
